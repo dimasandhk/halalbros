ab8cd310a0a08de4ae5623ea5a401af71c3f1d06
kodejalaid <70847887+kodejalaid@users.noreply.github.com>
2021-04-06 21:30:39 +0700

    About Component (2)

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,40 @@
 <template>
   <section class="about">
-    <h3 class="text-center">🚀 About & Credit</h3>
+    <div class="row justify-content-center">
+      <div class="col-12 col-md-10 col-lg-6">
+        <h3 class="text-center">🚀 About & Credit</h3>
+      </div>
+    </div>
+
+    <div class="row justify-content-center mt-4">
+      <div class="col-12 col-md-10 col-lg-6">
+        <h6
+          class="text-center text-muted"
+          v-for="({ msg }, i) of credit"
+          :key="i"
+        >
+          {{ msg }}
+        </h6>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    credit: [
+      { msg: "Made by Dimas Andhika Diputra" },
+      { msg: "Api by github.com/Zhirr (71%)" },
+      { msg: "Api by github.com/bachors (Al Quran)" },
+      { msg: "Api by github.com/lakuapik (Jadwal)" },
+    ],
+  }),
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.list-credit {
+  list-style: none;
+}
+</style>
